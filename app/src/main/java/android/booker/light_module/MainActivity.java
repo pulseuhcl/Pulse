7692a50a017e28224986cce0f7881cf052573d67
Eric Booker <eric90booker@gmail.com>
2019-03-25 22:34:44 -0500

    Code before testing.

--- app/src/main/java/android/booker/light_module/MainActivity.java
+++ app/src/main/java/android/booker/light_module/MainActivity.java
@@ -1,25 +1,30 @@
 package android.booker.light_module;
 
+import android.content.Context;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import java.util.Timer;
 import org.w3c.dom.Text;
+
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private Button flashButton;
     private EditText frequencyInput;
-    private GetTime timeObject;
-
-    // Event listener for FlashButton
-    private View.OnClickListener FlashButtonOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            FlashButtonClicked();
-        }
-    };
+    Boolean light = true;
+    CameraDevice cameraDevice;
+    private CameraManager cameraManager;
+    private CameraCharacteristics cameraCharacteristics;
+    String cameraID;
+    Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +33,48 @@ public class MainActivity extends AppCompatActivity {
         frequencyInput = findViewById(R.id.frequencyInputEditText);
         flashButton = findViewById(R.id.FlashButton);
         flashButton.setOnClickListener(FlashButtonOnClickListener);
+        getSystemService(Context.CAMERA_SERVICE);
+        try{
+            cameraID = cameraManager.getCameraIdList()[0];
+        }catch (CameraAccessException e){
+            e.printStackTrace();
+        }
     }
 
-    private void FlashButtonClicked(){
-        // gets the frequency that the user input into the EditText box
-        int frequency = Integer.parseInt(frequencyInput.getText().toString());
-        // gets the time one second ahead of the start time
-        int stopTime = timeObject.GetCurrentSecond() + 1;
-
-        while(frequency > 0){
-            if(stopTime != timeObject.GetCurrentSecond()){
-
-                frequency--;
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            if(light) {
+                try {
+                    cameraManager.setTorchMode(cameraID, true);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+                light=false;
             }
 
+            else{
+                try{
+                    cameraManager.setTorchMode(cameraID, false);
+                }catch(CameraAccessException e){
+                    e.printStackTrace();
+                }
+
+                light=true;
+            }
         }
+    };
 
 
+    private void FlashButtonClicked() {
+        timer.schedule(task, 1000, 1000);
     }
+
+    // Event listener for FlashButton
+    private View.OnClickListener FlashButtonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FlashButtonClicked();
+        }
+    };
 }
