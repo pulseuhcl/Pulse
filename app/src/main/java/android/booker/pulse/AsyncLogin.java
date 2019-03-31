98d0ffedabc5f3905783cd892e6c92456a6139f2
Eric Booker <eric90booker@gmail.com>
2019-03-31 02:40:35 -0500

    Initial commit

---
+++ app/src/main/java/android/booker/pulse/AsyncLogin.java
@@ -0,0 +1,119 @@
+package android.booker.pulse;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AsyncLogin extends AsyncTask<String, String, String> {
+    private Login_Page login_page;
+    HttpURLConnection conn;
+    URL url = null;
+
+    public AsyncLogin(Login_Page login_page){
+        this.login_page = login_page;
+    }
+
+    @Override
+    protected void onPreExecute(){
+        super.onPreExecute();
+        // optional progress bar would go here
+    }
+
+    @Override
+    protected String doInBackground(String... params){
+        try{
+            url = new URL("http://23e3b971.ngrok.io/pulse/login.php");
+        }catch(MalformedURLException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "exception";
+        }
+        try{
+            // Setup HttpUrlConnection class to send and receive data from php and mysql
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(10000);
+            conn.setRequestMethod("POST");
+
+            // setDoInput and set DoOutput method depict handling of both send/receive
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            // Append parameters to URL
+            Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("username", params[0])
+                    .appendQueryParameter("password", params[1]);
+            String query = builder.build().getEncodedQuery();
+
+            // Open connection for sending data
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(query);
+            writer.flush();
+            writer.close();
+            os.close();
+            conn.connect();
+        }catch(IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+            return "exception";
+        }
+        try{
+            int response_code = conn.getResponseCode();
+
+            // Check if successful connection made
+            if(response_code == HttpURLConnection.HTTP_OK){
+                // Read data sent from server
+                InputStream input = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                StringBuilder result = new StringBuilder();
+                String line = reader.readLine();
+
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+            // Pass data to onPostExecute method
+            return (result.toString());
+            }else{
+                return("unsuccessful");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+            return "exception";
+        }finally{
+            conn.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        // this method will be running on a UI thread
+
+        if(result.equalsIgnoreCase("true")){
+           Intent intent = new Intent(login_page, User_Logged_In.class);
+            login_page.startActivity(intent);
+            login_page.finish();
+            Toast.makeText(login_page, "Login success!", Toast.LENGTH_LONG).show();
+        } else if (result.equalsIgnoreCase("false")) {
+            // If username and password does not match display the error
+            Toast.makeText(login_page, "Login error!", Toast.LENGTH_LONG).show();
+        }
+        else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")){
+            Toast.makeText(login_page, "Error. Something went wrong!", Toast.LENGTH_LONG).show();
+        }
+    }
+}
