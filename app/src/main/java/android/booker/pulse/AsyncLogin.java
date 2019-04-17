4bbdfefd58e36e4b39ac281de2cedb07b5419c9d
Eric Booker <eric90booker@gmail.com>
2019-04-16 20:13:48 -0500

    Merge branch 'mireya' of https://github.com/pulseuhcl/Pulse into HEAD Something is wrong with the most recent commit on Mireya's branch. I rebased  using her previous commit before she added the registration page. I am still getting the following error: java.lang.RuntimeException: Canvas: trying to draw too large(180633600bytes) bitmap. I will commit again after this has been resolved. # Conflicts: #	.idea/misc.xml #	.idea/vcs.xml #	app/src/main/AndroidManifest.xml #	app/src/main/java/android/booker/pulse/AsyncLogin.java #	app/src/main/java/android/booker/pulse/Register_Page.java #	app/src/main/java/android/booker/pulse/User_Logged_In.java #	app/src/main/res/layout/activity_landing_page.xml #	app/src/main/res/layout/activity_login_page.xml #	app/src/main/res/layout/activity_profile_page.xml #	app/src/main/res/values/colors.xml #	app/src/main/res/values/strings.xml

--- app/src/main/java/android/booker/pulse/AsyncLogin.java
+++ app/src/main/java/android/booker/pulse/AsyncLogin.java
@@ -5,6 +5,10 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -34,7 +38,7 @@ public class AsyncLogin extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... params){
         try{
-            url = new URL("http://c6a5f93e.ngrok.io/pulse/login.php");
+            url = new URL("http://facedc8f.ngrok.io/pulse_api/login.php");
         }catch(MalformedURLException e){
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -90,8 +94,20 @@ public class AsyncLogin extends AsyncTask<String, String, String> {
                     result.append(line);
                     line = reader.readLine();
                 }
-            // Pass data to onPostExecute method
-            return (result.toString());
+                try{
+                    JSONArray jsonArray = new JSONArray(result.toString());
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        currentUser.setUserName(object.getString("username"));
+                        currentUser.setPassword(object.getString("password"));
+                        currentUser.setFirstName(object.getString("firstname"));
+                        currentUser.setLastName(object.getString("lastname"));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+                // Pass data to onPostExecute method
+                return (result.toString().trim());
             }else{
                 return("unsuccessful");
             }
@@ -107,14 +123,15 @@ public class AsyncLogin extends AsyncTask<String, String, String> {
     protected void onPostExecute(String result){
         // this method will be running on a UI thread
 
-        if(result.equalsIgnoreCase("true")){
-            Toast.makeText(login_page, "Login success!", Toast.LENGTH_LONG).show();
+        if(result.contains(currentUser.getPassword())){
+            Toast.makeText(login_page, result, Toast.LENGTH_LONG).show();
             // give a brief wait so the user can see the login success Toast
             Intent intent = new Intent(login_page, User_Logged_In.class);
             // this passes the serialized user object to the next activity for further use
+
             intent.putExtra("currentUser", currentUser);
             login_page.startActivity(intent);
-        } else if (result.equalsIgnoreCase("false")) {
+        }else if (!result.contains(currentUser.getPassword())) {
             // If username and password does not match display the error
             Toast.makeText(login_page, "Login error!", Toast.LENGTH_LONG).show();
         }
