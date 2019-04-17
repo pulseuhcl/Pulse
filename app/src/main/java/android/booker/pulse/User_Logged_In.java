3ee6c09b765f049741f2c3ef7194c51549413f5b
Eric Booker <eric90booker@gmail.com>
2019-04-17 00:13:13 -0500

    Widget constraints for the following views have been fixed: -Landing Page -User_Logged_In page -Registration Page -Login Page
    * Some widgets are still not positioned exactly as planned. This will
    need to be worked on please.
    
    Glide Library implemented to load the images into the containers. This
    was done because Java didn't work. The dependency was added accordingly.
    
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/android/booker/pulse/AsyncLogin.java
    #	app/src/main/java/android/booker/pulse/Register_Page.java
    #	app/src/main/java/android/booker/pulse/User_Logged_In.java
    #	app/src/main/res/layout/activity_landing_page.xml
    #	app/src/main/res/layout/activity_login_page.xml
    #	app/src/main/res/layout/activity_profile_page.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/android/booker/pulse/User_Logged_In.java
+++ app/src/main/java/android/booker/pulse/User_Logged_In.java
@@ -2,17 +2,47 @@ package android.booker.pulse;
 import java.io.Serializable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class User_Logged_In extends AppCompatActivity {
     User currentUser = new User();
+    private ImageView gearIcon;
+    private ImageView userIcon;
+    private ImageButton unlockButton;
+    private ImageButton sendCodeButton;
     private User_Logged_In userLoggedIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__logged__in);
         currentUser = getIntent().getExtras().getParcelable("currentUser");
-        //testing to see if the parcelable object was passed successfully via intent
+        gearIcon = findViewById(R.id.gearIcon);
+        userIcon = findViewById(R.id.userIcon);
+        unlockButton = findViewById(R.id.unlockButton);
+        sendCodeButton = findViewById(R.id.sendCodeButton);
+        //testing to see if the user object was passed successfully via intent
         Toast.makeText(getApplicationContext(), "username is " + currentUser.getUserName(), Toast.LENGTH_LONG).show();
+
+        Glide
+                .with(this)
+                .load(getDrawable(R.drawable.user))
+                .into(userIcon);
+        Glide
+                .with(this)
+                .load(getDrawable(R.drawable.gear))
+                .into(gearIcon);
+        Glide
+                .with(this)
+                .load(getDrawable(R.drawable.pulse))
+                .into(unlockButton);
+        Glide
+                .with(this)
+                .load(getDrawable(R.drawable.send))
+                .into(sendCodeButton);
     }
 }
